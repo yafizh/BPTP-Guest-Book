f6c140549ac30970300c062ae92538253d1bbc05
yafizh <a.chairil.yafizh@gmail.com>
2021-12-27 23:21:50 +0800

    remove some useless sidebar

--- index.php
+++ index.php
@@ -65,30 +65,6 @@
                 <span>Data Tamu</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link " href="form-components.html">
-                <i class="material-icons">view_module</i>
-                <span>Forms &amp; Components</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="tables.html">
-                <i class="material-icons">table_chart</i>
-                <span>Tables</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="user-profile-lite.html">
-                <i class="material-icons">person</i>
-                <span>User Profile</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="errors.html">
-                <i class="material-icons">error</i>
-                <span>Errors</span>
-              </a>
-            </li>
           </ul>
         </div>
       </aside>
