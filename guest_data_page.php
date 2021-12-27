59e7cd1d39964b767db2314be4561080a9b3c474
yafizh <a.chairil.yafizh@gmail.com>
2021-12-27 23:18:56 +0800

    make guest data page to show guest data and still not working

---
+++ guest_data_page.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html class="no-js h-100" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Shards Dashboard Lite - Free Bootstrap Admin Template – DesignRevision</title>
+    <meta name="description" content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="styles/shards-dashboards.1.1.0.min.css">
+    <link rel="stylesheet" href="styles/extras.1.1.0.min.css">
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+</head>
+
+<body class="h-100">
+
+    <div class="container-fluid">
+        <div class="row">
+            <!-- End Main Sidebar -->
+            <main class="main-content col-lg-10 col-md-9 col-sm-12 p-0 offset-lg-2 offset-md-3">
+                <div class="main-navbar sticky-top bg-white">
+                    <!-- Main Navbar -->
+                    <nav class="navbar align-items-stretch navbar-light flex-md-nowrap p-0">
+                        <form action="#" class="main-navbar__search w-100 d-none d-md-flex d-lg-flex">
+                            <div class="input-group input-group-seamless ml-3">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">
+                                        <i class="fas fa-search"></i>
+                                    </div>
+                                </div>
+                                <input class="navbar-search form-control" type="text" placeholder="Search for something..." aria-label="Search">
+                            </div>
+                        </form>
+                        <nav class="nav">
+                            <a href="#" class="nav-link nav-link-icon toggle-sidebar d-md-inline d-lg-none text-center border-left" data-toggle="collapse" data-target=".header-navbar" aria-expanded="false" aria-controls="header-navbar">
+                                <i class="material-icons">&#xE5D2;</i>
+                            </a>
+                        </nav>
+                    </nav>
+                </div>
+                <!-- / .main-navbar -->
+                <div class="main-content-container container-fluid px-4">
+                    <!-- Page Header -->
+                    <div class="page-header row no-gutters py-4">
+                        <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+                            <span class="text-uppercase page-subtitle">Fitur</span>
+                            <h3 class="page-title">Data Tamu</h3>
+                        </div>
+                    </div>
+                    <!-- End Page Header -->
+                    <!-- Default Light Table -->
+                    <div class="row">
+                        <div class="col">
+                            <div class="card card-small mb-4">
+                                <div class="card-header border-bottom">
+                                    <h6 class="mb-0">Seluruh Data Tamu</h6>
+                                </div>
+                                <div class="card-body p-0 pb-0 text-center">
+                                    <table class="table mb-0 table-hover">
+                                        <thead class="bg-light">
+                                            <tr>
+                                                <th scope="col" class="border-0">#</th>
+                                                <th scope="col" class="border-0">Nama Tamu</th>
+                                                <th scope="col" class="border-0">Tanggal</th>
+                                                <th scope="col" class="border-0">Asal Instansi</th>
+                                                <th scope="col" class="border-0">Bertemu</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Ali</td>
+                                                <td>Kerry</td>
+                                                <td>Russian Federation</td>
+                                                <td>Gdańsk</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Clark</td>
+                                                <td>Angela</td>
+                                                <td>Estonia</td>
+                                                <td>Borghetto di Vara</td>
+                                            </tr>
+                                            <tr>
+                                                <td>3</td>
+                                                <td>Jerry</td>
+                                                <td>Nathan</td>
+                                                <td>Cyprus</td>
+                                                <td>Braunau am Inn</td>
+                                            </tr>
+                                            <tr>
+                                                <td>4</td>
+                                                <td>Colt</td>
+                                                <td>Angela</td>
+                                                <td>Liberia</td>
+                                                <td>Bad Hersfeld</td>
+                                            </tr>
+                                            <tr>
+                                                <td>5</td>
+                                                <td>Colt</td>
+                                                <td>Angela</td>
+                                                <td>Liberia</td>
+                                                <td>Bad Hersfeld</td>
+                                            </tr>
+                                            <tr>
+                                                <td>6</td>
+                                                <td>Colt</td>
+                                                <td>Angela</td>
+                                                <td>Liberia</td>
+                                                <td>Bad Hersfeld</td>
+                                            </tr>
+                                            <tr>
+                                                <td>7</td>
+                                                <td>Colt</td>
+                                                <td>Angela</td>
+                                                <td>Liberia</td>
+                                                <td>Bad Hersfeld</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="card-footer border-top">
+                                    <div class="row">
+                                        <div class="col text-center view-report">
+                                            <nav aria-label="Page navigation example">
+                                                <ul class="pagination justify-content-center">
+                                                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                                                </ul>
+                                            </nav>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Default Light Table -->
+                </div>
+            </main>
+        </div>
+    </div>
--- index.php
+++ index.php
@@ -1,117 +1,124 @@
 <!doctype html>
 <html class="no-js h-100" lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Shards Dashboard Lite - Free Bootstrap Admin Template – DesignRevision</title>
-    <meta name="description" content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="styles/shards-dashboards.1.1.0.min.css">
-    <link rel="stylesheet" href="styles/extras.1.1.0.min.css">
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
-  </head>
-  <body class="h-100">
-    <div class="container-fluid">
-      <div class="row">
-        <!-- Main Sidebar -->
-        <aside class="main-sidebar col-12 col-md-3 col-lg-2 px-0">
-          <div class="main-navbar">
-            <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
-              <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
-                <div class="d-table m-auto">
-                  <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="images/shards-dashboards-logo.svg" alt="Shards Dashboard">
-                  <span class="d-none d-md-inline ml-1">Shards Dashboard</span>
-                </div>
-              </a>
-              <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
-                <i class="material-icons">&#xE5C4;</i>
-              </a>
-            </nav>
-          </div>
-          <form action="#" class="main-sidebar__search w-100 border-right d-sm-flex d-md-none d-lg-none">
-            <div class="input-group input-group-seamless ml-3">
-              <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <i class="fas fa-search"></i>
-                </div>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Shards Dashboard Lite - Free Bootstrap Admin Template – DesignRevision</title>
+  <meta name="description" content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="styles/shards-dashboards.1.1.0.min.css">
+  <link rel="stylesheet" href="styles/extras.1.1.0.min.css">
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+</head>
+
+<body class="h-100">
+  <div class="container-fluid">
+    <div class="row">
+      <!-- Main Sidebar -->
+      <aside class="main-sidebar col-12 col-md-3 col-lg-2 px-0">
+        <div class="main-navbar">
+          <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
+            <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
+              <div class="d-table m-auto">
+                <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="images/shards-dashboards-logo.svg" alt="Shards Dashboard">
+                <span class="d-none d-md-inline ml-1">Shards Dashboard</span>
               </div>
-              <input class="navbar-search form-control" type="text" placeholder="Search for something..." aria-label="Search"> </div>
-          </form>
-          <div class="nav-wrapper">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link <?= isset($_GET['page']) ? "" : "active"?>" href="index.php">
-                  <i class="material-icons">edit</i>
-                  <span>Beranda</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link <?= isset($_GET['page']) ? (($_GET['page'] == 'guest_book') ? "active": "") : ""?>" href="?page=guest_book">
-                  <i class="material-icons">vertical_split</i>
-                  <span>Buku Tamu</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="add-new-post.html">
-                  <i class="material-icons">note_add</i>
-                  <span>Data Tamu</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="form-components.html">
-                  <i class="material-icons">view_module</i>
-                  <span>Forms &amp; Components</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="tables.html">
-                  <i class="material-icons">table_chart</i>
-                  <span>Tables</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="user-profile-lite.html">
-                  <i class="material-icons">person</i>
-                  <span>User Profile</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link " href="errors.html">
-                  <i class="material-icons">error</i>
-                  <span>Errors</span>
-                </a>
-              </li>
-            </ul>
+            </a>
+            <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
+              <i class="material-icons">&#xE5C4;</i>
+            </a>
+          </nav>
+        </div>
+        <form action="#" class="main-sidebar__search w-100 border-right d-sm-flex d-md-none d-lg-none">
+          <div class="input-group input-group-seamless ml-3">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <i class="fas fa-search"></i>
+              </div>
+            </div>
+            <input class="navbar-search form-control" type="text" placeholder="Search for something..." aria-label="Search">
           </div>
-        </aside>
-        <!-- End Main Sidebar -->
-        <?php 
-          if(isset($_GET['page'])){
-            switch($_GET['page']){
-              case "guest_book":
-                include_once "guest_book_page.php";
-                break;
-              default:
-                include_once "page_not_found.html";
-            }
-          } else {
-            include_once 'home_page.php';
-          }
-        ?>
-      </div>
+        </form>
+        <div class="nav-wrapper">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link <?= isset($_GET['page']) ? "" : "active" ?>" href="index.php">
+                <i class="material-icons">edit</i>
+                <span>Beranda</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link <?= isset($_GET['page']) ? (($_GET['page'] == 'guest_book') ? "active" : "") : "" ?>" href="?page=guest_book">
+                <i class="material-icons">vertical_split</i>
+                <span>Buku Tamu</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link <?= isset($_GET['page']) ? (($_GET['page'] == 'guest_data') ? "active" : "") : "" ?>" href="?page=guest_data">
+                <i class="material-icons">note_add</i>
+                <span>Data Tamu</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="form-components.html">
+                <i class="material-icons">view_module</i>
+                <span>Forms &amp; Components</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="tables.html">
+                <i class="material-icons">table_chart</i>
+                <span>Tables</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="user-profile-lite.html">
+                <i class="material-icons">person</i>
+                <span>User Profile</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="errors.html">
+                <i class="material-icons">error</i>
+                <span>Errors</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+      <!-- End Main Sidebar -->
+      <?php
+      if (isset($_GET['page'])) {
+        switch ($_GET['page']) {
+          case "guest_book":
+            include_once "guest_book_page.php";
+            break;
+          case "guest_data":
+            include_once "guest_data_page.php";
+            break;
+          default:
+            include_once "page_not_found.html";
+        }
+      } else {
+        include_once 'home_page.php';
+      }
+      ?>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-    <script src="https://unpkg.com/shards-ui@latest/dist/js/shards.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Sharrre/2.0.1/jquery.sharrre.min.js"></script>
-    <script src="scripts/extras.1.1.0.min.js"></script>
-    <script src="scripts/shards-dashboards.1.1.0.min.js"></script>
-    <script src="scripts/app/app-blog-overview.1.1.0.js"></script>
-  </body>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+  <script src="https://unpkg.com/shards-ui@latest/dist/js/shards.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Sharrre/2.0.1/jquery.sharrre.min.js"></script>
+  <script src="scripts/extras.1.1.0.min.js"></script>
+  <script src="scripts/shards-dashboards.1.1.0.min.js"></script>
+  <script src="scripts/app/app-blog-overview.1.1.0.js"></script>
+</body>
+
 </html>
