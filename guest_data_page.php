8188d886ddf260a4e73c9eb4e5a08bf90eb20809
yafizh <a.chairil.yafizh@gmail.com>
2021-12-28 14:10:26 +0800

    add search place holder on guest_data_page.php

--- guest_data_page.php
+++ guest_data_page.php
@@ -1,24 +1,3 @@
-<!doctype html>
-<html class="no-js h-100" lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Shards Dashboard Lite - Free Bootstrap Admin Template – DesignRevision</title>
-    <meta name="description" content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="styles/shards-dashboards.1.1.0.min.css">
-    <link rel="stylesheet" href="styles/extras.1.1.0.min.css">
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-</head>
-
-<body class="h-100">
-
-    <div class="container-fluid">
-        <div class="row">
             <!-- End Main Sidebar -->
             <main class="main-content col-lg-10 col-md-9 col-sm-12 p-0 offset-lg-2 offset-md-3">
                 <div class="main-navbar sticky-top bg-white">
@@ -31,7 +10,7 @@
                                         <i class="fas fa-search"></i>
                                     </div>
                                 </div>
-                                <input class="navbar-search form-control" type="text" placeholder="Search for something..." aria-label="Search">
+                                <input class="navbar-search form-control" type="text" placeholder="Cari berdasarkan Nama Tamu, Tanggal, Asal Instansi, Bertemu..." aria-label="Search">
                             </div>
                         </form>
                         <nav class="nav">
@@ -143,5 +122,3 @@
                     <!-- End Default Light Table -->
                 </div>
             </main>
-        </div>
-    </div>
