12845c2bc2b241bf77c108c41852c8c8907584e1
yafizh <a.chairil.yafizh@gmail.com>
2022-01-13 06:57:49 +0800

    custumize layout

--- app/home/feature/home_page.php
+++ app/home/feature/home_page.php
@@ -83,27 +83,166 @@
         <!-- End Small Stats Blocks -->
         <div class="row">
             <!-- Users Stats -->
-            <div class="col-12 mb-4">
+            <div class="col-7 mb-4">
                 <div class="card card-small">
                     <div class="card-header border-bottom">
                         <h6 class="m-0">Grafik Pengunjung </h6>
                     </div>
                     <div class="card-body pt-0">
-                        <canvas height="75" style="max-width: 100% !important;" class="blog-overview-users"></canvas>
+                        <canvas height="220" style="max-width: 100% !important;" class="blog-overview-users"></canvas>
                     </div>
                 </div>
             </div>
             <!-- End Users Stats -->
+            <div class="col-lg-5 mb-4">
+                <div class="row">
+                    <!-- Top Referrals Component -->
+                    <div class="col-lg-7 col-md-12 col-sm-12 mb-4">
+                        <div class="stats-small stats-small--1 card card-small">
+                            <div class="card-header border-bottom">
+                                <h6 class="m-0">Jumlah Pengunjung</h6>
+                            </div>
+                            <div class="card-body p-3 d-flex">
+                                <div class="d-flex flex-column m-auto">
+                                    <div class="stats-small__data text-center">
+                                        <span class="stats-small__label text-uppercase">1 - 12 Januari</span>
+                                        <h6 class="stats-small__value count my-3">10</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer border-top">
+                                <div class="row">
+                                    <div class="col-7">
+                                        <div id="blog-overview-date-range" class="input-daterange input-group input-group-sm my-auto ml-auto mr-auto ml-sm-auto mr-sm-0" style="max-width: 350px;">
+                                            <input type="text" class="input-sm form-control" name="start" placeholder="Start Date" id="blog-overview-date-range-1">
+                                            <input type="text" class="input-sm form-control" name="end" placeholder="End Date" id="blog-overview-date-range-2">
+                                            <span class="input-group-append">
+                                                <span class="input-group-text">
+                                                    <i class="material-icons"></i>
+                                                </span>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="col text-right view-report">
+                                        <a href="#">Cetak Laporan &rarr;</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Top Referrals Component -->
+                    <!-- Top Referrals Component -->
+                    <div class="col-lg-5 col-md-12 col-sm-12 mb-4">
+                        <div class="stats-small stats-small--1 card card-small">
+                            <div class="card-header border-bottom">
+                                <h6 class="m-0">Jumlah Admin</h6>
+                            </div>
+                            <div class="card-body p-3 d-flex">
+                                <div class="d-flex flex-column m-auto">
+                                    <div class="stats-small__data text-center">
+                                        <span class="stats-small__label text-uppercase">Admin</span>
+                                        <h6 class="stats-small__value count my-3">1</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer border-top">
+                                <div class="row">
+                                    <div class="col text-center view-report">
+                                        <a href="#" class="btn btn-white">Cetak Data Admin</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Top Referrals Component -->
+                </div>
+                <div class="row">
+                    <!-- Top Referrals Component -->
+                    <div class="col-lg-12 col-md-12 col-sm-12 mb-4">
+                        <div class="card card-small">
+                            <div class="card-header border-bottom">
+                                <h6 class="m-0">Jumlah Pengunjung Pegawai</h6>
+                            </div>
+                            <div class="card-body p-0">
+                                <ul class="list-group list-group-small list-group-flush">
+                                    <li class="list-group-item d-flex px-3">
+                                        <span class="text-semibold text-fiord-blue">Eko</span>
+                                        <span class="ml-auto text-right text-semibold text-reagent-gray">19,291</span>
+                                    </li>
+                                    <li class="list-group-item d-flex px-3">
+                                        <span class="text-semibold text-fiord-blue">Stack Overflow</span>
+                                        <span class="ml-auto text-right text-semibold text-reagent-gray">11,201</span>
+                                    </li>
+                                    <li class="list-group-item d-flex px-3">
+                                        <span class="text-semibold text-fiord-blue">Hacker News</span>
+                                        <span class="ml-auto text-right text-semibold text-reagent-gray">9,291</span>
+                                    </li>
+                                    <li class="list-group-item d-flex px-3">
+                                        <span class="text-semibold text-fiord-blue">Reddit</span>
+                                        <span class="ml-auto text-right text-semibold text-reagent-gray">8,281</span>
+                                    </li>
+                                    <li class="list-group-item d-flex px-3">
+                                        <span class="text-semibold text-fiord-blue">The Next Web</span>
+                                        <span class="ml-auto text-right text-semibold text-reagent-gray">7,128</span>
+                                    </li>
+                                    <li class="list-group-item d-flex px-3">
+                                        <span class="text-semibold text-fiord-blue">Tech Crunch</span>
+                                        <span class="ml-auto text-right text-semibold text-reagent-gray">6,218</span>
+                                    </li>
+                                    <li class="list-group-item d-flex px-3">
+                                        <span class="text-semibold text-fiord-blue">YouTube</span>
+                                        <span class="ml-auto text-right text-semibold text-reagent-gray">1,218</span>
+                                    </li>
+                                    <li class="list-group-item d-flex px-3">
+                                        <span class="text-semibold text-fiord-blue">Adobe</span>
+                                        <span class="ml-auto text-right text-semibold text-reagent-gray">827</span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="card-footer border-top">
+                                <div class="row">
+                                    <div class="col">
+                                        <select class="custom-select custom-select-sm">
+                                            <option selected>Minggu Lalu</option>
+                                            <option value="1">Hari ini</option>
+                                            <option value="2">Bulan Lalu</option>
+                                            <option value="3">Tahun Lalu</option>
+                                        </select>
+                                    </div>
+                                    <div class="col text-right view-report">
+                                        <a href="#">Cetak Laporan &rarr;</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Top Referrals Component -->
+                </div>
+            </div>
         </div>
     </div>
+    <div id="aaa">asd</div>
 </main>
+<script type="text/javascript">
+    $("#aaa").on("click", function() {
+        var divContents = "asdasdad";
+        var printWindow = window.open('', '', 'height=400,width=800');
+        printWindow.document.write('<html><head><title>DIV Contents</title>');
+        printWindow.document.write('</head><body >');
+        printWindow.document.write(divContents);
+        printWindow.document.write('</body></html>');
+        printWindow.document.close();
+        printWindow.print();
+    });
+</script>
 <script>
     // https://stackoverflow.com/questions/13009755/getjson-synchronous
     $.ajaxSetup({
         async: false
     });
 
-
+    // Blog overview date range init.
+    $('#blog-overview-date-range').datepicker({});
 
 
     let guestToday;
@@ -265,93 +404,93 @@
 
     // Data
     var bouData = {
-      // Generate the days labels on the X axis.
-      labels: guestThisMonth['guest_this_month'].map(value => parseInt(value.guest_visit_day)),
-      datasets: [{
-        label: 'Bulan ini',
-        fill: 'start',
-        data: guestThisMonth['guest_this_month'].map(value => parseInt(value.guest_visit_date_count)),
-        backgroundColor: 'rgba(0,123,255,0.1)',
-        borderColor: 'rgba(0,123,255,1)',
-        pointBackgroundColor: '#ffffff',
-        pointHoverBackgroundColor: 'rgb(0,123,255)',
-        borderWidth: 1.5,
-        pointRadius: 0,
-        pointHoverRadius: 3
-      }, {
-        label: 'Bulan lalu',
-        fill: 'start',
-        data: guestLastMonth['guest_last_month'].map(value => parseInt(value.guest_visit_date_count)),
-        backgroundColor: 'rgba(255,65,105,0.1)',
-        borderColor: 'rgba(255,65,105,1)',
-        pointBackgroundColor: '#ffffff',
-        pointHoverBackgroundColor: 'rgba(255,65,105,1)',
-        borderDash: [3, 3],
-        borderWidth: 1,
-        pointRadius: 0,
-        pointHoverRadius: 2,
-        pointBorderColor: 'rgba(255,65,105,1)'
-      }]
+        // Generate the days labels on the X axis.
+        labels: guestThisMonth['guest_this_month'].map(value => parseInt(value.guest_visit_day)),
+        datasets: [{
+            label: 'Bulan ini',
+            fill: 'start',
+            data: guestThisMonth['guest_this_month'].map(value => parseInt(value.guest_visit_date_count)),
+            backgroundColor: 'rgba(0,123,255,0.1)',
+            borderColor: 'rgba(0,123,255,1)',
+            pointBackgroundColor: '#ffffff',
+            pointHoverBackgroundColor: 'rgb(0,123,255)',
+            borderWidth: 1.5,
+            pointRadius: 0,
+            pointHoverRadius: 3
+        }, {
+            label: 'Bulan lalu',
+            fill: 'start',
+            data: guestLastMonth['guest_last_month'].map(value => parseInt(value.guest_visit_date_count)),
+            backgroundColor: 'rgba(255,65,105,0.1)',
+            borderColor: 'rgba(255,65,105,1)',
+            pointBackgroundColor: '#ffffff',
+            pointHoverBackgroundColor: 'rgba(255,65,105,1)',
+            borderDash: [3, 3],
+            borderWidth: 1,
+            pointRadius: 0,
+            pointHoverRadius: 2,
+            pointBorderColor: 'rgba(255,65,105,1)'
+        }]
     };
 
     // Options
     var bouOptions = {
-      responsive: true,
-      legend: {
-        position: 'top'
-      },
-      elements: {
-        line: {
-          // A higher value makes the line look skewed at this ratio.
-          tension: 0.3
+        responsive: true,
+        legend: {
+            position: 'top'
         },
-        point: {
-          radius: 0
+        elements: {
+            line: {
+                // A higher value makes the line look skewed at this ratio.
+                tension: 0.3
+            },
+            point: {
+                radius: 0
+            }
+        },
+        scales: {
+            xAxes: [{
+                gridLines: false,
+                ticks: {
+                    callback: function(tick, index) {
+                        // Jump every 7 values on the X axis labels to avoid clutter.
+                        return index % 7 !== 0 ? '' : tick;
+                    }
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    suggestedMax: 45,
+                    callback: function(tick, index, ticks) {
+                        if (tick === 0) {
+                            return tick;
+                        }
+                        // Format the amounts using Ks for thousands.
+                        return tick > 999 ? (tick / 1000).toFixed(1) + 'K' : tick;
+                    }
+                }
+            }]
+        },
+        // Uncomment the next lines in order to disable the animations.
+        // animation: {
+        //   duration: 0
+        // },
+        hover: {
+            mode: 'nearest',
+            intersect: false
+        },
+        tooltips: {
+            custom: false,
+            mode: 'nearest',
+            intersect: false
         }
-      },
-      scales: {
-        xAxes: [{
-          gridLines: false,
-          ticks: {
-            callback: function (tick, index) {
-              // Jump every 7 values on the X axis labels to avoid clutter.
-              return index % 7 !== 0 ? '' : tick;
-            }
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            suggestedMax: 45,
-            callback: function (tick, index, ticks) {
-              if (tick === 0) {
-                return tick;
-              }
-              // Format the amounts using Ks for thousands.
-              return tick > 999 ? (tick/ 1000).toFixed(1) + 'K' : tick;
-            }
-          }
-        }]
-      },
-      // Uncomment the next lines in order to disable the animations.
-      // animation: {
-      //   duration: 0
-      // },
-      hover: {
-        mode: 'nearest',
-        intersect: false
-      },
-      tooltips: {
-        custom: false,
-        mode: 'nearest',
-        intersect: false
-      }
     };
 
     // Generate the Analytics Overview chart.
     window.BlogOverviewUsers = new Chart(bouCtx, {
-      type: 'LineWithLine',
-      data: bouData,
-      options: bouOptions
+        type: 'LineWithLine',
+        data: bouData,
+        options: bouOptions
     });
 
     // Hide initially the first and last analytics overview chart points.
