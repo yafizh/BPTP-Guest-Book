9043fd6a749ec35f1b0c71742a979cdca9df7787
yafizh <a.chairil.yafizh@gmail.com>
2022-01-27 09:08:28 +0800

    change brand and logo

--- app/index.php
+++ app/index.php
@@ -41,8 +41,8 @@
           <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
             <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
               <div class="d-table m-auto">
-                <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="<?= BASE_URL ?>images/shards-dashboards-logo.svg" alt="Shards Dashboard">
-                <span class="d-none d-md-inline ml-1">Shards Dashboard</span>
+                <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="<?= BASE_URL ?>images/bptp-logo.png" alt="Shards Dashboard">
+                <span class="d-none d-md-inline ml-1">BPTP KALSEL</span>
               </div>
             </a>
             <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
